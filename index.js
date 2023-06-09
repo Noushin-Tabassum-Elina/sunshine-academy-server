433d689100d48b1e1da7aa688569a07502678e83
Noushin-Tabassum-Elina <elinajsr05@gmail.com>
2023-06-10 02:06:53 +0600

    all users route added

--- index.js
+++ index.js
@@ -16,49 +16,67 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
 
-    const classCollection = client.db('sunshineDb').collection('class');
-    const userCollection = client.db('sunshinwDb').collection('users');
-
-   
-
-    
+        const classCollection = client.db('sunshineDb').collection('class');
+        const userCollection = client.db('sunshinwDb').collection('users');
 
 
 
 
 
 
+        //getting all user
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            // const existingUser = await usersCollection.findOne(query);
+
+            // if (existingUser) {
+            //   return res.send({ message: 'user already exists' })
+            // }
+
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
 app.get('/', (req, res) => {
-  res.send('Summer camp is starting')
+    res.send('Summer camp is starting')
 })
 
 app.listen(port, () => {
-  console.log(`Summer is starting on port ${port}`);
+    console.log(`Summer is starting on port ${port}`);
 })
