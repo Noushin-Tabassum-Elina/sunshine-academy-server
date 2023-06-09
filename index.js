fd569b59a778de638ace72238eb4a42a109954bc
Noushin-Tabassum-Elina <elinajsr05@gmail.com>
2023-06-10 02:05:29 +0600

    successfully connected with mongodb

--- index.js
+++ index.js
@@ -11,6 +11,47 @@ app.use(express.json());
 
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hbpsi8j.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const classCollection = client.db('sunshineDb').collection('class');
+    const userCollection = client.db('sunshinwDb').collection('users');
+
+   
+
+    
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
